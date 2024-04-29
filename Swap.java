ce19befad57ae8a85e30eb857ee239a350440e42
Nirbhay <nirbhaysingh943012@gmail.com>
2024-04-29 08:45:09 +0530

    swap using third variable

---
+++ Swap.java
@@ -0,0 +1,10 @@
+public class Swap {
+    public static void main(String[] args) {
+        int n = 10, m = 20;
+        int temp = n;
+        n = m;
+        m = temp;
+        System.out.println("n = " + n);
+        System.out.println("m = " + m);
+    }
+}

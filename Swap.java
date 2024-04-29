6096d7759714d4424aeed62d3c481dcade0232cb
Nirbhay <nirbhaysingh943012@gmail.com>
2024-04-29 08:56:28 +0530

    swap without using third variable

--- Swap.java
+++ Swap.java
@@ -1,9 +1,22 @@
 public class Swap {
     public static void main(String[] args) {
+        // swap with thitd variable
         int n = 10, m = 20;
-        int temp = n;
-        n = m;
-        m = temp;
+        System.out.println("Before swapping:");
+        System.out.println("n = " + n);
+        System.out.println("m = " + m);
+        // int temp = n;
+        // n = m;
+        // m = temp;
+        // System.out.println("\nAfter swapping:");
+        // System.out.println("n = " + n);
+        // System.out.println("m = " + m);
+
+        // swap without using third variable
+        n = n + m;
+        m = n - m;
+        n = n - m;
+        System.out.println("\nAfter swapping:");
         System.out.println("n = " + n);
         System.out.println("m = " + m);
     }

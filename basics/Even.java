411645cbf7b78bc56fc2d70e9475d3667399bdaa
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-10-23 14:00:00 +0530

    even odd

--- basics/Even.java
+++ basics/Even.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Even {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
         int x = sc.nextInt();
         if(x % 2 == 0) {
             System.out.println("Even");
